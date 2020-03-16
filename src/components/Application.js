35a1163ca10591c7b749feae0492cb1dd68cf9f1
Berk Ozer <galipberkozer@gmail.com>
2020-03-16 18:15:54 -0400

    Bring in mock days data into Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,24 @@ import React from "react";
 
 import "components/Application.scss";
 
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
 export default function Application(props) {
   return (
     <main className="layout">
