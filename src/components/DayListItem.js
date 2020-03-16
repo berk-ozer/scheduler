bd82fa17d90ffdd97c737da7540061b07b99f22b
Berk Ozer <galipberkozer@gmail.com>
2020-03-16 17:51:05 -0400

    Create function formatSpots which returns a text displaying the number of spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,13 +9,24 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   })
 
+  const formatSpots = (numberOfSpots) => {
+    switch(numberOfSpots) {
+      case 0:
+        return 'no spots remaining';
+      case 1:
+        return '1 spot remaining';
+      default:
+        return `${numberOfSpots} spots remaining`
+    }
+  }
+
   return (
     <li 
       className={dayClass}
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 } 
