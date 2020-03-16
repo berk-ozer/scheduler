a77458a777bf8ef2a47608cd7637010dcf43e0fa
Berk Ozer <galipberkozer@gmail.com>
2020-03-16 18:34:08 -0400

    Add state to keep track of current day, pass day and setDay to DayList

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import "components/DayList";
@@ -23,6 +23,8 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -35,8 +37,8 @@ export default function Application(props) {
       <nav className="sidebar__menu">
         <DayList
           days={days}
-          day={"Monday"}
-          setDay={day => console.log(day)}
+          day={day}
+          setDay={setDay}
         />
       </nav>
       <img
