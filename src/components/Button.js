88e531d932c27d3186e0935d20116c5ec6daf9e4
Berk Ozer <galipberkozer@gmail.com>
2020-03-16 15:30:31 -0400

    Conditionally add className to button to style accordingly (confirm, danger etc.)

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,16 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
+   let buttonClass = 'button';
+
+   if (props.confirm) {
+      buttonClass += ' button--confirm';
+   }
+   if (props.danger) {
+      buttonClass += ' button--danger';
+   }
+
    return (
-      <button>{props.children}</button>
+      <button className={buttonClass}>{props.children}</button>
    );
 }
