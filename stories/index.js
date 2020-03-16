c9295f4319ba7cffb2a3cdf4d9f9d1b8a8e780a7
Berk Ozer <galipberkozer@gmail.com>
2020-03-16 17:06:26 -0400

    Bring in the stories for DayListItem component

--- stories/index.js
+++ stories/index.js
@@ -23,3 +23,14 @@ storiesOf("Button", module)
       Disabled
     </Button>
   ));
+
+  storiesOf("DayListItem", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    })
+    .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
+    .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+    .add("Full", () => <DayListItem name="Monday" spots={0} />)
+    .add("Clickable", () => (
+      <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
+    ));
