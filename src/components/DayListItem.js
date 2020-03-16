8b0f8000a9ab8fd6877c6c3b3a654168a27c7a79
Berk Ozer <galipberkozer@gmail.com>
2020-03-16 17:24:18 -0400

    Conditionally create classes for DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,19 @@
 import React from "react";
+import classnames from "classnames";
 
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected" : props.selected,
+    "day-list__item--full": props.spots === 0
+  })
 
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li 
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
