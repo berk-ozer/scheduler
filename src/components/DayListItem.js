75fbdb33a56babf2986b5521125e770fdb9779d1
Berk Ozer <galipberkozer@gmail.com>
2020-03-16 16:47:07 -0400

    Create basic DayListItem component, import to stories/index.js

---
+++ src/components/DayListItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DayListItem(props) {
+  return (
+    <li>
+      <h2 className="text--regular"></h2>
+      <h3 className="text--light"></h3>
+    </li>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -6,6 +6,7 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+import DayListItem from "components/DayListItem";
 
 storiesOf("Button", module)
   .addParameters({
