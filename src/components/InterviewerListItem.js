c331eccdfe05995b02609d59dc3115dd81c0319f
Berk Ozer <galipberkozer@gmail.com>
2020-03-17 14:20:25 -0400

    Only show interviewer name if the item is selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -18,7 +18,7 @@ export default function InterviewerListItem(props) {
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   )
 }
