88804a6794e001373e9e43de15f269ed8c6df00b
Berk Ozer <galipberkozer@gmail.com>
2020-03-17 19:31:45 -0400

    Pass in student and interviewer props to Show

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -12,7 +12,10 @@ export default function Appointment(props) {
       <Header time={props.time} />
       {
       props.interview ?
-      <Show/> :
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      /> :
       <Empty/>
       }
     </article>
