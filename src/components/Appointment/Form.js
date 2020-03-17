03c580559dc8854fe0ede38c86694f87a93a346b
Berk Ozer <galipberkozer@gmail.com>
2020-03-17 18:32:58 -0400

    useState to track name, use controlled component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,6 +4,8 @@ import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -13,7 +15,8 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
+            value={name}
+            onChange={event => setName(event.target.value)}
           />
         </form>
         <InterviewerList 
