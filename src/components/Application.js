43367aa7267af342fc1e07eb862b08204798c5c3
Berk Ozer <galipberkozer@gmail.com>
2020-03-17 19:47:57 -0400

    Create mock appointments data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
-import "components/Application.scss";
-import "components/DayList";
+
 import DayList from "components/DayList";
+import Appointment from "components/Appointment"
+
+import "components/Application.scss";
 
 const days = [
   {
@@ -20,6 +22,53 @@ const days = [
     name: "Wednesday",
     spots: 0,
   },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Berk Ozer",
+      interviewer: {
+        id: 2,
+        name: "Jessica Jones",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Sarah English",
+      interviewer: {
+        id: 3,
+        name: "Justin Goebow",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
 ];
 
 export default function Application(props) {
