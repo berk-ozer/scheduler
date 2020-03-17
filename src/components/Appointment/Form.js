591f4a40114798f153e9e247fa1d7b5015137dbd
Berk Ozer <galipberkozer@gmail.com>
2020-03-17 19:05:45 -0400

    Pass name and interviewer to props.onSave

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -41,7 +41,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
