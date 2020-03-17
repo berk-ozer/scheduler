338747e32dd0b64ce316c2317ded54fe82d47cde
Berk Ozer <galipberkozer@gmail.com>
2020-03-17 19:01:43 -0400

    Prevent default form submission behaviour

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -20,7 +20,9 @@ export default function Form(props) {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form 
+          autoComplete="off" 
+          onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
