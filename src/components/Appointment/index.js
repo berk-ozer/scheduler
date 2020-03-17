0144a46c7298497c3f5bfcb542ba48652e55c6a5
Berk Ozer <galipberkozer@gmail.com>
2020-03-17 16:01:28 -0400

    Create skeleton Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "Appointment/styles.scss"
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      
+    </article>
+  )
+}
