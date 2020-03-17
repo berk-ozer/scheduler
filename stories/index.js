4bcc485ff4b942aaf0c598e42bb40c6b1b02d7ef
Berk Ozer <galipberkozer@gmail.com>
2020-03-17 14:41:02 -0400

    Edit story for InterviewerListItem to pass in cb function

--- stories/index.js
+++ stories/index.js
@@ -120,7 +120,7 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={event => action("setInterviewer")(interviewer.id)}
     />
   ));
 
