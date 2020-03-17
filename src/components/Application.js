65d42203e999a90d107fb92e340fee2108c5cd6e
Berk Ozer <galipberkozer@gmail.com>
2020-03-17 19:54:46 -0400

    Render Appointment components using mock data

--- src/components/Application.js
+++ src/components/Application.js
@@ -97,7 +97,13 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment
+            key={appointment.id}
+            {...appointment}
+          />
+        ))}
+        <Appointment id="last" time="5pm" />
       </section>
     </main>
   );
