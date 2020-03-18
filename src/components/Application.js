7f3a66049286c178f25690730c1fd6615c268ec6
Berk Ozer <galipberkozer@gmail.com>
2020-03-18 16:35:46 -0400

    Fetch days data from server and store it in state

--- src/components/Application.js
+++ src/components/Application.js
@@ -59,6 +59,11 @@ export default function Application(props) {
   const [day, setDay] = useState("Monday");
   const [days, setDays] = useState([]);
 
+  useEffect(() => {
+    axios.get("/api/days")
+      .then(response => setDays(response.data));
+  }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
