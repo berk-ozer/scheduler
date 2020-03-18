3601babd54f8eacd5c3c7a62d6a1481fc964baa2
Berk Ozer <galipberkozer@gmail.com>
2020-03-18 19:19:09 -0400

    Build getAppointmentsForDay selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,19 @@
 export const getAppointmentsForDay = (state, day) => {
+  const dayObj = state.days.find(elem => elem.name === day);
 
+  if (!dayObj) {
+    return [];
+  }
+
+  const appointmentIds = dayObj.appointments;
+
+  const appointmentsForDay = [];
+
+  for (const id in state.appointments) {
+    if (appointmentIds.includes(Number(id))) {
+      appointmentsForDay.push(state.appointments[id])
+    }
+  }
+
+  return appointmentsForDay;
 }
