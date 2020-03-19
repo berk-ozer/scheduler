28ceddd28578eb6e38bc8926a8506cc2ac6a46a2
Berk Ozer <galipberkozer@gmail.com>
2020-03-19 14:56:26 -0400

    Bring in test for back limit of useVisualMode

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -36,3 +36,10 @@ test("useVisualMode should return to previous mode", () => {
   act(() => result.current.back());
   expect(result.current.mode).toBe(FIRST);
 });
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
