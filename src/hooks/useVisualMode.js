6a98f89f822802eb4010ba3ca700b8a9e0806bb1
Berk Ozer <galipberkozer@gmail.com>
2020-03-19 14:15:55 -0400

    Bring in mode transition test for useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,7 +3,10 @@ import React, { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
+  const transition = newMode => setMode(newMode);
+
   return {
     mode,
+    transition
   }
 }
