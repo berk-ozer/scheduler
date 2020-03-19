85e2ec68196cc7f626249187a8a3680ce8d43821
Berk Ozer <galipberkozer@gmail.com>
2020-03-19 09:54:53 -0400

    Build getInterview selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -17,3 +17,24 @@ export const getAppointmentsForDay = (state, day) => {
 
   return appointmentsForDay;
 }
+
+
+
+export const getInterview = (state, interview) => {
+  if (!interview) {
+    return null;
+  }
+
+  const interviewerId = interview.interviewer;
+
+  for (const id in state.interviewers) {
+    if (Number(id) === interviewerId) {
+      return (
+        {
+          student: interview.student,
+          interviewer: state.interviewers[id]
+        }
+      )
+    }
+  }
+}
