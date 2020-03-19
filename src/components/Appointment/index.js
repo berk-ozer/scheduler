dde721ffc920c11c7e04b4ae651a582089d9cff1
Berk Ozer <galipberkozer@gmail.com>
2020-03-19 19:43:42 -0400

    Build save function that calls props.bookInterview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -20,6 +20,19 @@ export default function Appointment(props) {
       props.interview ? SHOW : EMPTY
     );
 
+
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    props.bookInterview(props.id, interview);
+  }
+
+
+
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -40,6 +53,7 @@ export default function Appointment(props) {
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
+          onSave={save}
           onCancel={back}
         />
       )}
