b719fc2362d808531671cf730527c8060f4b4013
Berk Ozer <galipberkozer@gmail.com>
2020-03-19 16:40:38 -0400

    Build optional parameter "replace" into transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import { useState } from "react";
+
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = newMode => {
+
+
+  const transition = (newMode, replace = false) => {
     setMode(newMode);
-    setHistory(history => [...history, newMode]);
+
+    if (replace) {
+      setHistory(history => {
+        const newHistory = [...history];
+        newHistory.splice(- 1, 1, newMode);
+        return newHistory;
+      }) 
+    } else {
+      setHistory(history => [...history, newMode]);
+    }
   } 
+
+
 
   const back = () => {
     setHistory(history => {
       const newHistory = history.length > 1 ? [...history].slice(0, -1) : [...history];
+
       setMode(newHistory[newHistory.length - 1]);
+
       return newHistory;
     })
   }
+
+
 
   return {
     mode,
