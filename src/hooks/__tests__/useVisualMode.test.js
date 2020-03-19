c74cc1d424577f0cc7db6be400c9e81acd47a1d6
Berk Ozer <galipberkozer@gmail.com>
2020-03-19 14:23:31 -0400

    Bring in test for back function of useVisualMode

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -6,6 +6,7 @@ import useVisualMode from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
 const SECOND = "SECOND";
+const THIRD = "THIRD";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
@@ -18,4 +19,20 @@ test("useVisualMode should transition to another mode", () => {
 
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
+});
+
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 });
