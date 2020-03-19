58fda62840dfe5e293d1524b92ce9fe07c981a43
Berk Ozer <galipberkozer@gmail.com>
2020-03-18 20:26:09 -0400

    Remove appointments mock data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,57 +3,11 @@ import React, { useState, useEffect } from "react";
 
 import DayList from "components/DayList";
 import Appointment from "components/Appointment"
+import getAppointmentsForDay from "helpers/selectors"
 
 import axios from "axios";
 
 import "components/Application.scss";
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Berk Ozer",
-      interviewer: {
-        id: 2,
-        name: "Jessica Jones",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Sarah English",
-      interviewer: {
-        id: 3,
-        name: "Justin Goebow",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  }
-];
 
 export default function Application(props) {
   const [state, setState] = useState({
