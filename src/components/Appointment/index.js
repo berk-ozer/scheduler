8a040d9316687e0bc5cc7243d1e39e725bab7a7c
Berk Ozer <galipberkozer@gmail.com>
2020-03-19 17:08:17 -0400

    useVisualMode in Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,21 +3,31 @@ import React from "react";
 import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
+import useVisualMode from "hooks/useVisualMode";
 
 import "./styles.scss"
 
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
+
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+      props.interview ? SHOW : EMPTY
+    );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {
-      props.interview ?
-      <Show
+      {mode === EMPTY && <Empty/>}
+      {mode === SHOW && (
+        <Show
         student={props.interview.student}
         interviewer={props.interview.interviewer}
-      /> :
-      <Empty/>
-      }
+        /> 
+      )}
     </article>
   )
 }
