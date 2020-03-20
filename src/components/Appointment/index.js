277d0a67c1470dab947188d06b8491d3d1861c31
Berk Ozer <galipberkozer@gmail.com>
2020-03-20 09:21:53 -0400

    Once bookInterview promise resolves, transition to "show" mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -28,9 +28,8 @@ export default function Appointment(props) {
       interviewer
     };
 
-    props.bookInterview(props.id, interview);
-
-    transition(SHOW);
+    props.bookInterview(props.id, interview)
+      .then(() => transition(SHOW));
   }
 
 
