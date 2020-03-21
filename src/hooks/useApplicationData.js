0cdfb54af51d53ad16f758cab9a63552166364f0
Berk Ozer <galipberkozer@gmail.com>
2020-03-21 17:18:00 -0400

    Refactor to use useReducer instead of useState

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,19 +1,60 @@
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import axios from "axios";
 
 
 
 
+const SET_DAY = "SET_DAY";
+const SET_DAYS = "SET_DAYS";
+const SET_APP_DATA = "SET_APP_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+
+
+
+function reducer(state, action) {
+  const reducers = {
+    [SET_DAY]: function(state, value) {
+      return ({ ...state, ...value });
+    },
+
+    [SET_APP_DATA]: function(state, value) {
+      return ({ ...state, ...value});
+    }, 
+
+    [SET_DAYS]: function(state, value) {
+      return ({ ...state, days: value})
+    },
+
+    [SET_INTERVIEW]: function(state, value) {
+      const appointment = {
+        ...state.appointments[value.id],
+        interview: {...value.interview}
+      }
+      const appointments = {
+        ...state.appointments,
+        [value.id]: appointment
+      }
+      return ({ ...state, appointments});
+    }
+  }
+
+  return reducers[action.type](state, action.value) || state;
+}
+
+
+
+
+
 export default function useApplicationData() {
-  const [state, setState] = useState({
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
   })
 
-  const setDay = day => setState(state => ({...state, day}));
-
+  const setDay = day => dispatch({ type: SET_DAY, value: {day} });
 
 
 
@@ -22,9 +63,7 @@ export default function useApplicationData() {
       axios.get("/api/days"),
       axios.get("/api/appointments"),
       axios.get("/api/interviewers")
-    ]).then(all => {
-      setState(state => ({...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    })
+    ]).then(all => dispatch({ type: SET_APP_DATA, value: { days: all[0].data, appointments: all[1].data, interviewers: all[2].data }}));
   }, []);
 
 
@@ -33,43 +72,23 @@ export default function useApplicationData() {
   // Updates remaining spots in days after any appointment is changed 
   useEffect(() => {
     axios.get("/api/days")
-      .then(days => setState(state => ({ ...state, days: days.data })));
+      .then(days => dispatch({ type: SET_DAYS, value: days.data }));
   }, [state.appointments])
 
 
 
 
   function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: {...interview}
-    }
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-
     return axios.put(`/api/appointments/${id}`, {interview})
-      .then(response => setState(state => ({ ...state, appointments })));
+      .then(response => dispatch({ type: SET_INTERVIEW, value: {id, interview} }));
   }
 
 
 
 
   function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-
     return axios.delete(`/api/appointments/${id}`)
-      .then(response => setState(state => ({ ...state, appointments })));
+      .then(response => dispatch({ type: SET_INTERVIEW, value: {id, interview: null} }));
   }
 
 
