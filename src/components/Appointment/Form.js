852646e960ca73b77230942f33ae34b2db217908
Berk Ozer <galipberkozer@gmail.com>
2020-03-23 16:54:41 -0400

    Validate if name is in input field before saving

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,19 +3,36 @@ import React, { useState } from "react";
 import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
+
+
+
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
 
   const reset = () => {
     setName("");
     setInterviewer(null);
   }
 
+
   const cancel = () => {
     reset();
     props.onCancel();
   }
+
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
+  }
+
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -33,6 +50,7 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers} 
           value={interviewer} 
@@ -42,7 +60,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
