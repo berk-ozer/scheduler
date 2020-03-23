28c7bde53646c97db99886b17ef09fdae83df051
Berk Ozer <galipberkozer@gmail.com>
2020-03-23 17:34:07 -0400

    Add test to simulate user entering name into input

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -53,4 +53,18 @@ describe("Form", () => {
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
+
+
+  it("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render(<Form interviewers={interviewers} onSave={onSave} />);
+  
+    const input = getByPlaceholderText("Enter Student Name");
+  
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+  
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
 });
