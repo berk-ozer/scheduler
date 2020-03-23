1d2732f8dfb1389ca62ae68beb5c6910fb3ad6d3
Berk Ozer <galipberkozer@gmail.com>
2020-03-23 16:22:57 -0400

    Build basic tests for Form

---
+++ src/components/__tests__/Form.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Form from "components/Appointment/Form";
+
+afterEach(cleanup);
+
+
+
+
+
+describe("Form", () => {
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    }
+  ];
+
+  it("renders without student name if not provided", () => {
+    const { getByPlaceholderText } = render(<Form interviewers={interviewers}/>);
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  });
+
+  it("renders with initial student name", () => {
+    const { getByTestId } = render(<Form interviewers={interviewers} name="Lydia Miller-Jones"/>);
+    expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  });
+});
