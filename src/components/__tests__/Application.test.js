de974904f16279ca26cbb8dae7e05076dfe18fda
Berk Ozer <galipberkozer@gmail.com>
2020-03-24 09:58:51 -0400

    Change test syntax to use async/await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -8,12 +8,12 @@ afterEach(cleanup);
 
 
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday"))
-    .then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+  
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
