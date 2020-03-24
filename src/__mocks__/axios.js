069c58b1c0747c750858482e4b5bb76e4f1ad755
Berk Ozer <galipberkozer@gmail.com>
2020-03-24 09:23:53 -0400

    Build mock axios.get method

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -52,3 +52,33 @@ const fixtures = {
     }
   }
 };
+
+
+
+export default {
+  get: jest.fn(url => {
+    if (url === "/api/days") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days
+      });
+    }
+
+    if (url === "/api/appointments") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      })
+    }
+
+    if (url === "/api/interviewers") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      })
+    }
+  })
+}
