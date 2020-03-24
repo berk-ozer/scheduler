e0bbb5e04b21d79ae300de9a51704fbbe9fffaa3
Berk Ozer <galipberkozer@gmail.com>
2020-03-24 17:37:22 -0400

    Add test for canceling an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -22,7 +22,7 @@ describe("Application", () => {
   
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     // Render component and wait for data to load
-    const { container, debug } = render(<Application/>);
+    const { container } = render(<Application/>);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -44,13 +44,39 @@ describe("Application", () => {
 
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
 
-    const day = getAllByTestId(container, "day").find(day => 
-      queryByText(day, "Monday")
-    );
 
+
+    // const day = getAllByTestId(container, "day").find(day => 
+    //   queryByText(day, "Monday")
+    // );
     // expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
 
     // Not doing the above validation
+    // "spots remaining" functionality uses server side logic. My app fetches "days" with updated "spots" every time there is an interview scheduled or deleted
+    // Talked to a mentor and they suggested I don't test for this here, because it's server side logic
+  });
+
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // Render component and wait for data to load
+    const { container } = render(<Application/>);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment").find(appointment => 
+        queryByText(appointment, "Archie Cohen")
+      );
+
+    // Cancel the interview and validate
+    fireEvent.click(getByAltText(appointment, "Delete"));
+    expect(getByText(appointment, /Are you sure/i)).toBeInTheDocument();
+
+    fireEvent.click(getByText(appointment, "Confirm"));
+    expect(getByText(appointment, ("Deleting"))).toBeInTheDocument();
+
+    await waitForElement(() => getByAltText(appointment, "Add"));
+
+    // Not doing "spots remaining" testing
     // "spots remaining" functionality uses server side logic. My app fetches "days" with updated "spots" every time there is an interview scheduled or deleted
     // Talked to a mentor and they suggested I don't test for this here, because it's server side logic
   });
