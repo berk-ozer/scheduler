1820cb9c19875448ce6a106089255b966238c2f9
Berk Ozer <galipberkozer@gmail.com>
2020-03-24 14:36:34 -0400

    Start building test on booking an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -8,12 +8,38 @@ afterEach(cleanup);
 
 
 
-it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
-
-  await waitForElement(() => getByText("Monday"));
-
-  fireEvent.click(getByText("Tuesday"));
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
   
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    // Render component and wait for data to load
+    const { container } = render(<Application/>);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+
+    // Book an interview
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(container, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+
+    console.log(prettyDOM(appointment));
+  });
+})
