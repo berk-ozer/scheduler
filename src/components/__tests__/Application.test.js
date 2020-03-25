1589f4fc7bf7094467b6402b85c0fa13e90ec72a
Berk Ozer <galipberkozer@gmail.com>
2020-03-25 17:17:14 -0400

    Comments

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,7 +9,7 @@ import {
   queryByText,
   getAllByTestId,
   getByAltText,
-  getByPlaceholderText,
+  getByPlaceholderText
 } from "@testing-library/react";
 
 import axios from "axios";
@@ -20,16 +20,18 @@ afterEach(cleanup);
 
 describe("Application", () => {
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    // Render component and wait for data to load
     const { getByText } = render(<Application />);
 
     await waitForElement(() => getByText("Monday"));
 
+    // Click "Tuesday" and validate
     fireEvent.click(getByText("Tuesday"));
 
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
-  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  it("loads data and books an interview", async () => {
     // Render component and wait for data to load
     const { container } = render(<Application />);
 
@@ -41,7 +43,7 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Add"));
 
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-      target: { value: "Lydia Miller-Jones" },
+      target: { value: "Lydia Miller-Jones" }
     });
 
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
@@ -52,18 +54,9 @@ describe("Application", () => {
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
 
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
-
-    // const day = getAllByTestId(container, "day").find(day =>
-    //   queryByText(day, "Monday")
-    // );
-    // expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
-
-    // Not doing the above validation
-    // "spots remaining" functionality uses server side logic. My app fetches "days" with updated "spots" every time there is an interview scheduled or deleted
-    // Talked to a mentor and they suggested I don't test for this here, because it's server side logic
   });
 
-  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  it("loads data and cancels an interview", async () => {
     // Render component and wait for data to load
     const { container } = render(<Application />);
 
@@ -72,7 +65,7 @@ describe("Application", () => {
     const appointment = getAllByTestId(
       container,
       "appointment"
-    ).find((appointment) => queryByText(appointment, "Archie Cohen"));
+    ).find(appointment => queryByText(appointment, "Archie Cohen"));
 
     // Cancel the interview and validate
     fireEvent.click(getByAltText(appointment, "Delete"));
@@ -82,13 +75,9 @@ describe("Application", () => {
     expect(getByText(appointment, "Deleting")).toBeInTheDocument();
 
     await waitForElement(() => getByAltText(appointment, "Add"));
-
-    // Not doing "spots remaining" testing
-    // "spots remaining" functionality uses server side logic. My app fetches "days" with updated "spots" every time there is an interview scheduled or deleted
-    // Talked to a mentor and they suggested I don't test for this here, because it's server side logic
   });
 
-  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+  it("loads data and edits an interview", async () => {
     // Render component and wait for data to load
     const { container } = render(<Application />);
 
@@ -97,13 +86,13 @@ describe("Application", () => {
     const appointment = getAllByTestId(
       container,
       "appointment"
-    ).find((appointment) => queryByText(appointment, "Archie Cohen"));
+    ).find(appointment => queryByText(appointment, "Archie Cohen"));
 
     // Edit the interview
     fireEvent.click(getByAltText(appointment, "Edit"));
 
     fireEvent.change(getByPlaceholderText(appointment, "Enter Student Name"), {
-      target: { value: "Berk Ozer" },
+      target: { value: "Berk Ozer" }
     });
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
@@ -114,10 +103,6 @@ describe("Application", () => {
 
     await waitForElement(() => getByText(appointment, "Berk Ozer"));
     expect(getByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
-
-    // Not doing "spots remaining" testing
-    // "spots remaining" functionality uses server side logic. My app fetches "days" with updated "spots" every time there is an interview scheduled or deleted
-    // Talked to a mentor and they suggested I don't test for this here, because it's server side logic
   });
 
   it("shows the save error when failing to save an appointment", async () => {
@@ -134,7 +119,7 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Add"));
 
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-      target: { value: "Lydia Miller-Jones" },
+      target: { value: "Lydia Miller-Jones" }
     });
 
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
@@ -166,7 +151,7 @@ describe("Application", () => {
     const appointment = getAllByTestId(
       container,
       "appointment"
-    ).find((appointment) => queryByText(appointment, "Archie Cohen"));
+    ).find(appointment => queryByText(appointment, "Archie Cohen"));
 
     // Try to delete an appointment
     fireEvent.click(getByAltText(appointment, "Delete"));
