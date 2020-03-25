9562430ae59c178ab4c5ce4f898f7c21659259ae
Berk Ozer <galipberkozer@gmail.com>
2020-03-25 15:51:51 -0400

    E2E test for booking an interview

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+
+describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+    cy.visit("/");
+    cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+});
