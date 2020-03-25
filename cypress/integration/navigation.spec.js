98e09a7e6f13f335e6fc230890f6fd774118a8cb
Berk Ozer <galipberkozer@gmail.com>
2020-03-25 15:04:44 -0400

    Test to select a day

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,14 @@
+/* eslint-disable no-undef */
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
