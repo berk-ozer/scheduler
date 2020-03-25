ee9009d8da87ead7433d018dc6fba3ca839ccd07
Berk Ozer <galipberkozer@gmail.com>
2020-03-25 16:12:02 -0400

    Don't skip tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -7,7 +7,7 @@ describe("Appointments", () => {
     cy.contains("Monday");
   });
 
-  xit("should book an interview", () => {
+  it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
       .click();
@@ -21,7 +21,7 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
-  xit("should edit an interview", () => {
+  it("should edit an interview", () => {
     cy.get("[alt=Edit]").click({ force: true });
 
     cy.get("[data-testid=student-name-input]")
