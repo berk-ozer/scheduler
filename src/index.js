32fcccd6f2ec16f8f95a5cf3a294c2362c784c96
Berk Ozer <galipberkozer@gmail.com>
2020-04-08 16:48:12 -0400

    Set the baseURL for axios as the Heroku API server, if it exists

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "index.scss";
 
 import Application from "components/Application";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(<Application />, document.getElementById("root"));
